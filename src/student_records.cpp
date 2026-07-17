1686661d9036d770632909dcadf56cd636d0fe87
RobertFbanks <robert@robertfairbanks.com>
2026-07-17 00:10:05 +0000

    Submition For student_records.cpp

--- src/student_records.cpp
+++ src/student_records.cpp
@@ -4,16 +4,28 @@
 using namespace std;
 
 bool isValidStudentId(string id) {
-    // TODO:
+
     // A valid ID should have at least 3 characters.
     // A valid ID should start with an uppercase letter.
     // Hint: You may compare characters directly, such as id[0] >= 'A'.
-    return false;
+
+    if ((id.length() >= 3) && (id[0] >= 'A' && id[0] <= 'Z')){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 bool isValidScore(double score) {
-    // TODO: Return true when score is between 0 and 100, inclusive.
-    return false;
+
+    // Return true when score is between 0 and 100, inclusive.
+    if (0 <= score && score <= 100){
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 void printStudent(const Student& student) {
@@ -36,33 +48,73 @@ void printStudents(const Student students[], int size) {
 }
 
 double calculateAverageScore(const Student students[], int size) {
-    // TODO:
+
     // If the array is null or size is invalid, return 0.0.
     // Otherwise, return the average score.
-    return 0.0;
+    if (students == nullptr || size <= 0) {
+        return 0.0;
+    }
+    double totalscore = 0;
+        for (int i = 0; i < size; i++) {
+            
+            totalscore += students[i].score;
+    }
+    return totalscore / size;
 }
 
 double findHighestScore(const Student students[], int size) {
-    // TODO:
+
     // If the array is null or size is invalid, return 0.0.
     // Otherwise, return the highest score.
-    return 0.0;
+    if (students == nullptr || size <= 0) {
+        return 0.0;
+    }
+    double highscore = students[0].score;
+    for (int i = 1; i < size; i++) {
+        if(students[i].score > highscore){
+            highscore = students[i].score;
+        }
+    }
+    return highscore;
 }
 
 int findStudentById(const Student students[], int size, string targetId) {
-    // TODO:
+
     // Search by comparing students[i].id to targetId.
     // Return the index of the matching student.
     // Return -1 if no student is found.
+    if (students == nullptr || size <= 0) {
+        return -1;
+    }
+    for (int i = 0; i < size; i++) {
+        if (students[i].id == targetId) {
+            return i;
+        }
+    }
     return -1;
 }
 
 char determineLetterGrade(double score) {
-    // TODO:
+
     // Return 'A' for scores 90 or higher.
     // Return 'B' for scores 80 or higher.
     // Return 'C' for scores 70 or higher.
     // Return 'D' for scores 60 or higher.
     // Return 'F' otherwise.
-    return 'F';
+    if (score >= 90){
+        return 'A';
+    }
+    if (score >= 80){
+        return 'B';
+    }
+    if (score >= 70){
+        return 'C';
+    }
+    if (score >= 60){
+        return 'D';
+    }
+    else{
+        return 'F';
+    }
+    
 }
